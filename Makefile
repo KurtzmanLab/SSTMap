be99014620e5bfcc3a90cfe2d79044f73f3df253
kamran haider <kamranhaider.mb@gmail.com>
2017-04-29 03:56:48 -0400

    added makefile for entropy codes

---
+++ Makefile
@@ -0,0 +1,19 @@
+CC      = g++
+SOURCEDIR = scripts
+INSTALLDIR = ~/anaconda2/bin
+bruteclust: $(SOURCEDIR)/make_clust_brute.cpp
+	$(CC) -o bruteclust $(SOURCEDIR)/make_clust_brute.cpp; mv bruteclust $(INSTALLDIR)
+
+
+kdhsa102: $(SOURCEDIR)/kdhsa102_main.cpp $(SOURCEDIR)/kdhsa102.h
+	$(CC) -o kdhsa102 $(SOURCEDIR)/kdhsa102.cpp $(SOURCEDIR)/kdhsa102_main.cpp; mv kdhsa102 $(INSTALLDIR)
+
+6dimprobable: $(SOURCEDIR)/6dim_main.cpp $(SOURCEDIR)/6dimprobable.h
+	$(CC) -o 6dimprobable $(SOURCEDIR)/6dimprobable.cpp $(SOURCEDIR)/6dim_main.cpp; mv 6dimprobable $(INSTALLDIR)
+
+all: bruteclust kdhsa102 6dimprobable
+
+clean:
+	rm -f bruteclust
+	rm -f kdhsa102
+	rm -f 6dimprobable
--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import setup, Extension, find_packages
 import numpy
+import subprocess
+import os
 # define the extension module
-ext_module = Extension('_sstmap_ext', 
+extensions = []
+extensions.append(Extension('_sstmap_ext', 
 					sources=['sstmap/_sstmap_ext.c'], 
-					include_dirs=[numpy.get_include()])
+					include_dirs=[numpy.get_include()]))
+
+extensions.append(Extension('_sstmap_bruteclust', 
+                              sources=['scripts/make_clust_brute.cpp'], 
+                              language='c++'))
+
 
 setup(name='sstmap',
       version='0.1',
@@ -12,14 +20,23 @@ setup(name='sstmap',
       author='Kamran Haider',
       author_email='kamranhaider.mb@gmail.com',
       license='None',
-      ext_modules=[ext_module],
+      ext_modules=extensions,
       zip_safe=False,
       packages=find_packages(),
-      )
-entry_points={
+      entry_points={
       'console_scripts':
       ['run_hsa = sstmap.scripts.run_hsa:entry_point',
       'run_gist = sstmap.scripts.run_gist:entry_point',
       'desmond_extract_nbparams = sstmap.scripts.desmond_extract_nbparams:entry_point',
       'dtr_to_netcdf = sstmap.scripts.dtr_to_netcdf:entry_point',]},
+      )
 
+#g++ -o /usr/local/bin/kdhsa102 $RECIPE_DIR/../../scripts/kdhsa102.cpp $RECIPE_DIR/../../scripts/kdhsa102_main.cpp
+#g++ -o /usr/local/bin/6dimprobable $RECIPE_DIR/../../scripts/6dimprobable.cpp $RECIPE_DIR/../../scripts/6dim_main.cpp
+#g++ -o bruteclust make_clust_brute.cpp
+c_prog = os.path.abspath("scripts/make_clust_brute.cpp")
+try:
+      subprocess.check_call("g++  -o /usr/local/bin/bruteclust " + c_prog)
+except Exception as e:
+      print(e)
+print("g++  -o /usr/local/bin/bruteclust " + c_prog)
