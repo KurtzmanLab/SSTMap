f11e11c50284c2cb5f2961e92c178fc9e27c6aad
kamran haider <kamranhaider.mb@gmail.com>
2017-05-16 12:37:57 -0400

    setting up travis-CI

--- devtools/travis-ci/deploy_anaconda.sh
+++ devtools/travis-ci/deploy_anaconda.sh
@@ -9,7 +9,7 @@ echo "Converting conda package..."
 conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
 
 echo "Deploying to Anaconda.org..."
-anaconda -t $ANACONDA_TOKEN upload conda-bld/**/PACKAGENAME-*.tar.bz2
+anaconda -t $CONDA_UPLOAD_TOKEN upload conda-bld/**/PACKAGENAME-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
