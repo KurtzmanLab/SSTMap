bfb45ab3f116855cf6af5bbcc736cf2141b7cf8d
kamran haider <kamranhaider.mb@gmail.com>
2018-01-26 12:31:39 -0500

    fix libgsl issue in travis config

--- devtools/travis-ci/install_miniconda.sh
+++ devtools/travis-ci/install_miniconda.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+pushd .
+cd $HOME
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
-
+# Install Miniconda
+MINICONDA=Miniconda2-latest-Linux-x86_64.sh
+MINICONDA_HOME=$HOME/miniconda
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget https://repo.continuum.io/miniconda/$MINICONDA
+wget -q http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b
-rm -f $MINICONDA
+bash $MINICONDA -b -p $MINICONDA_HOME
 
-export PATH=$HOME/miniconda3/bin:$PATH
+# Configure miniconda
+export PIP_ARGS="-U"
+export PATH=$MINICONDA_HOME/bin:$PATH
+conda update --yes conda
+conda install --yes conda-build anaconda-client
 
-conda update -yq conda
-conda install -yq conda-build
+# Restore original directory
+popd
