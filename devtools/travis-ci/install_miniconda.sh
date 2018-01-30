807c6b7a3686922ac955de9e2169c44a84ed40f6
kamran haider <kamranhaider.mb@gmail.com>
2018-01-29 23:09:32 -0500

    started fixing travisCI scripts

--- devtools/travis-ci/install_miniconda.sh
+++ devtools/travis-ci/install_miniconda.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
-pushd .
-cd $HOME
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
 
-# Install Miniconda
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget -q http://repo.continuum.io/miniconda/$MINICONDA
+wget https://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b -p $MINICONDA_HOME
+bash $MINICONDA -b
+rm -f $MINICONDA
 
-# Configure miniconda
-export PIP_ARGS="-U"
-export PATH=$MINICONDA_HOME/bin:$PATH
-conda update --yes conda
-conda install --yes conda-build anaconda-client
+export PATH=$HOME/miniconda3/bin:$PATH
 
-# Restore original directory
-popd
-
-
+conda update -yq conda
+conda install -yq conda-build
