ff7596f585712f653951fd52f63c509e9bac914e
kamran haider <kamranhaider.mb@gmail.com>
2018-02-01 16:36:02 -0500

    testing anaconda deployment

--- devtools/travis-ci/deploy_anaconda.sh
+++ devtools/travis-ci/deploy_anaconda.sh
@@ -1,27 +1,11 @@
-#!/bin/bash
-# this script uses the ANACONDA_TOKEN env var.
-# to create a token:
-# >>> anaconda login
-# >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
-set -e
-
-echo "Converting conda package..."
-conda convert --platform all $HOME/miniconda2/conda-bld/linux-64/PACKAGENAME-*.tar.bz2 --output-dir conda-bld/
-
-echo "Deploying to Anaconda.org..."
-anaconda -t $CONDA_UPLOAD_TOKEN upload conda-bld/**/PACKAGENAME-*.tar.bz2
-
-echo "Successfully deployed to Anaconda.org."
-exit 0
-
-# Only need to change these two variables
-PKG_NAME=sstmap
-USER=solvationtools
-
-OS=$TRAVIS_OS_NAME-64
-mkdir ~/conda-bld
-conda config --set anaconda_upload no
-export CONDA_BLD_PATH=~/conda-bld
-export VERSION=`date +%Y.%m.%d`
-conda build .
-anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
+#!/usr/bin/env bash
+# Deploy to anaconda solvation tools channel
+conda install --yes anaconda-client
+pushd .
+cd $HOME/miniconda/conda-bld
+FILES=*/${PACKAGENAME}-dev-*.tar.bz2
+for filename in $FILES; do
+    anaconda -t $CONDA_UPLOAD_TOKEN remove --force ${ORGNAME}/${PACKAGENAME}-dev/${filename}
+    anaconda -t $CONDA_UPLOAD_TOKEN upload --force -u ${ORGNAME} -p ${PACKAGENAME}-dev ${filename}
+done
+popd
