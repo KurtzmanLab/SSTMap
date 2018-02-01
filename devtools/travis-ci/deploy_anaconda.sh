bad906d76d790bdf2222696c944e6a0c46df3b6e
kamran haider <kamranhaider.mb@gmail.com>
2018-02-01 16:19:28 -0500

    added version variable in meta.yml

--- devtools/travis-ci/deploy_anaconda.sh
+++ devtools/travis-ci/deploy_anaconda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this script uses the ANACONDA_TOKEN env var. 
+# this script uses the ANACONDA_TOKEN env var.
 # to create a token:
 # >>> anaconda login
 # >>> anaconda auth -c -n travis --max-age 307584000 --url https://anaconda.org/USERNAME/PACKAGENAME --scopes "api:write api:read"
@@ -13,3 +13,15 @@ anaconda -t $CONDA_UPLOAD_TOKEN upload conda-bld/**/PACKAGENAME-*.tar.bz2
 
 echo "Successfully deployed to Anaconda.org."
 exit 0
+
+# Only need to change these two variables
+PKG_NAME=sstmap
+USER=solvationtools
+
+OS=$TRAVIS_OS_NAME-64
+mkdir ~/conda-bld
+conda config --set anaconda_upload no
+export CONDA_BLD_PATH=~/conda-bld
+export VERSION=`date +%Y.%m.%d`
+conda build .
+anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-`date +%Y.%m.%d`-0.tar.bz2 --force
