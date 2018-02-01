75bb399521bb45807f7534db716d2c936ab4b5c4
kamran haider <kamranhaider.mb@gmail.com>
2018-02-01 17:39:06 -0500

    testing anaconda deployment

--- devtools/travis-ci/deploy_anaconda.sh
+++ devtools/travis-ci/deploy_anaconda.sh
@@ -2,10 +2,14 @@
 # Deploy to anaconda solvation tools channel
 conda install --yes anaconda-client
 pushd .
-cd $HOME/miniconda/conda-bld
-FILES=*/${PACKAGENAME}-dev-*.tar.bz2
+cd $HOME/travis/miniconda3/conda-bld
+ls
+ls */
+FILES=*/${PACKAGENAME}-*.tar.bz2
 for filename in $FILES; do
     anaconda -t $CONDA_UPLOAD_TOKEN remove --force ${ORGNAME}/${PACKAGENAME}-dev/${filename}
     anaconda -t $CONDA_UPLOAD_TOKEN upload --force -u ${ORGNAME} -p ${PACKAGENAME}-dev ${filename}
 done
 popd
+
+#anaconda upload /home/travis/miniconda3/conda-bld/linux-64/sstmap-1.1.0-py36_0.tar.bz2
