6ede7b255c36a7ce383ace4a3a458d9326038132
kamran haider <kamranhaider.mb@gmail.com>
2018-02-02 03:30:19 -0500

    testing anaconda deployment

--- devtools/travis-ci/deploy_anaconda.sh
+++ devtools/travis-ci/deploy_anaconda.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # Deploy to anaconda solvation tools channel
-conda install --yes anaconda-client
+# conda install --yes anaconda-client
 pushd .
-cd $HOME/travis/miniconda3/conda-bld
+cd $HOME/miniconda/conda-bld
 ls
 ls */
-FILES=*/${PACKAGENAME}-*.tar.bz2
+FILES=*/${PACKAGENAME}*.tar.bz2
 for filename in $FILES; do
     anaconda -t $CONDA_UPLOAD_TOKEN remove --force ${ORGNAME}/${PACKAGENAME}-dev/${filename}
     anaconda -t $CONDA_UPLOAD_TOKEN upload --force -u ${ORGNAME} -p ${PACKAGENAME}-dev ${filename}
--- devtools/travis-ci/install_miniconda.sh
+++ devtools/travis-ci/install_miniconda.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
-
+# Install Miniconda
+MINICONDA=Miniconda2-latest-Linux-x86_64.sh
+MINICONDA_HOME=$HOME/miniconda
 MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget https://repo.continuum.io/miniconda/$MINICONDA
+wget -q http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b
-rm -f $MINICONDA
+bash $MINICONDA -b -p $MINICONDA_HOME
 
-export PATH=$HOME/miniconda3/bin:$PATH
+# Configure miniconda
+export PIP_ARGS="-U"
+export PATH=$MINICONDA_HOME/bin:$PATH
+conda update --yes conda
+conda install --yes conda-build=2.1.17 jinja2 anaconda-client pip
 
-conda update -yq conda
-conda install -yq conda-build=3.2 jinja2 anaconda-client
+# Restore original directory
+popd
+# OLD script
+#if [[ "$TRAVIS_OS_NAME" == "osx" ]];   then MINICONDA=Miniconda3-latest-MacOSX-x86_64.sh; fi
+#if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then MINICONDA=Miniconda3-latest-Linux-x86_64.sh;  fi
+
+#MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+#wget https://repo.continuum.io/miniconda/$MINICONDA
+#if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
+#    echo "Miniconda MD5 mismatch"
+#    exit 1
+#fi
+#bash $MINICONDA -b
+#rm -f $MINICONDA
+
+#export PATH=$HOME/miniconda3/bin:$PATH
+
+#conda update -yq conda
+#conda install -yq conda-build=3.2 jinja2 anaconda-client
+
+# Temporarily change directory to $HOME to install software
+#pushd .
+#cd $HOME
