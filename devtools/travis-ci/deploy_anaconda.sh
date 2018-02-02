6e335aae67fc7a0c4f7074de0c8debe39f3baacc
kamran haider <kamranhaider.mb@gmail.com>
2018-02-02 16:18:21 -0500

    fixed import issue in conda recipe for macos

--- devtools/travis-ci/deploy_anaconda.sh
+++ devtools/travis-ci/deploy_anaconda.sh
@@ -3,8 +3,6 @@
 # conda install --yes anaconda-client
 pushd .
 cd $HOME/miniconda/conda-bld
-ls
-ls */
 FILES=*/${PACKAGENAME}*.tar.bz2
 for filename in $FILES; do
     anaconda -t $CONDA_UPLOAD_TOKEN remove --force ${ORGNAME}/${PACKAGENAME}/${filename}
