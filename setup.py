b5d2262658fbadaec9f59ae2af27529c30e52013
Anthony Cruz <acruzpr@users.noreply.github.com>
2019-04-08 15:06:54 -0400

    Travis fix 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ extensions = []
 extensions.append(Extension('_sstmap_ext',
                             sources=['sstmap/_sstmap_ext.c'],
                             include_dirs=[numpy.get_include()],
-                            extra_link_args=['-lgsl','-lgslcblas','-L/lib/x86_64-linux-gnu/']))
+                            extra_link_args=['-lgsl','-lgslcblas','-L/usr/lib/x86_64-linux-gnu/']))
 
 extensions.append(Extension('_sstmap_entropy',
                             sources=['sstmap/_sstmap_entropy.cpp', 'sstmap/kdhsa102.cpp'],
