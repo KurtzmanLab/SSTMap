99d2cb1b5dec734c14448816cc84b475d3803bcf
Anthony Cruz <anthonycruzpr@gmil.com>
2021-01-18 09:59:22 -0500

    Improvements to setup (python < 3.8 and numpy <= 1.17.5)

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
 from setuptools import setup, Extension, find_packages
-import numpy
+import sys
+
+if sys.version_info.major == 3 and sys.version_info.minor >= 8:
+    sys.stderr.write("\nRuntimeError: Python 3.7.x or lower is required.\n"
+                     "You are using Python {}.{}.{}."
+                     "Please Install using Python version lower than Python 3.8.\n\n".format(sys.version_info.major,
+                                                                                             sys.version_info.minor,
+                                                                                             sys.version_info.micro))
+    sys.exit(1)
+
+try:
+    import numpy
+except ImportError as e:
+    sys.stderr.write("\n{}: Numpy needed for installation. "
+                     "Please install numpy <= 1.17.5.\n\n".format(type(e).__name__))
+    sys.exit(1)
+
+if '1.17' not in numpy.__version__ and int(numpy.__version__.split(".")[2]) > 17:
+    sys.stderr.write("\nRuntimeError: Numpy <= 1.17.5 needed for installation."
+                     "Please downgrade your numpy and install numpy <= 1.17.5.\n\n")
+    sys.exit(1)
+
 
 __version__ = "1.1.4"
 
@@ -25,7 +46,9 @@ setup(name='sstmap',
       license='MIT',
       url='https://github.com/KurtzmanLab/SSTMap',
       platforms=['Linux', 'Mac OS X',],
-      install_requires=['parmed==3.2.0','matplotlib==2.2.3'],
+      python_requires='<3.8',
+      install_requires=['parmed==3.2.0','matplotlib==2.2.3','mdtraj','numpy<=1.17.5'],
+      setup_requires=['numpy<=1.17.5'],
       packages=find_packages(),
       ext_modules=extensions,
       zip_safe=False,
