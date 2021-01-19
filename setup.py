0aa1e11733f6c1b93ccd1fa5063cc7231c78124a
Anthony Cruz <anthonycruzpr@gmil.com>
2021-01-19 15:02:13 -0500

    Check GCC and G++ version. Improved error messages.

--- setup.py
+++ setup.py
@@ -1,24 +1,58 @@
 from setuptools import setup, Extension, find_packages
+from os import environ
+from subprocess import check_output
 import sys
+import shlex
 
+
+# Check if we are using Python <= 3.7.x
 if sys.version_info.major == 3 and sys.version_info.minor >= 8:
-    sys.stderr.write("\nRuntimeError: Python 3.7.x or lower is required.\n"
-                     "You are using Python {}.{}.{}."
-                     "Please Install using Python version lower than Python 3.8.\n\n".format(sys.version_info.major,
-                                                                                             sys.version_info.minor,
-                                                                                             sys.version_info.micro))
+    sys.stderr.write("\nRuntimeError: Python 3.7.x or lower is required for SSTMap installation.\n"
+                     "You are using Python {}.{}.{}.\n"
+                     "Please Install a version of Python lower than Python 3.8.\n\n".format(sys.version_info.major,
+                                                                                            sys.version_info.minor,
+                                                                                            sys.version_info.micro))
     sys.exit(1)
 
+# Check C and C+ compiler version < 8.0
+if environ.get('CC') is None:
+    cc_ver_command_line = "gcc -dumpversion"
+else:
+    cc_ver_command_line = "{} -dumpversion".format(environ.get('CC'))
+
+cc_ver_command = shlex.split(cc_ver_command_line)
+cc_ver = None
+try:
+    cc_ver = int(check_output(cc_ver_command, encoding='utf-8').strip())
+except FileNotFoundError:
+    sys.stderr.write("\nFileNotFoundError: GCC and G++ compiler version < 8.0 needed for installation.\n"
+                     "Please install GCC and G++ compiler version < 8.0.\n\n")
+
+    sys.exit(1)
+
+if cc_ver > 7:
+    sys.stderr.write("\nRuntimeError: GCC and G++ compiler version < 8.0 needed for installation.\n"
+                     "Please install GCC and G++  compile version < 8.0.\n"
+                     "Set CC environment variable to the gcc executable (version < 8.0) and\n"
+                     "set CXX environment variable to the g++ executable (version < 8.0).\n\n"
+                     "Ex. If you install gcc and g++ version 7 then: \n"
+                     "    export CC=/usr/bin/gcc-7\n"
+                     "    export CXX=/usr/bin/g++-7\n\n")
+
+    sys.exit(1)
+
+# Check if Numpy is installed and if is Numpy <= 1.17.5
 try:
     import numpy
-except ImportError as e:
-    sys.stderr.write("\n{}: Numpy needed for installation. "
-                     "Please install numpy <= 1.17.5.\n\n".format(type(e).__name__))
+except ImportError:
+    sys.stderr.write("\nModuleNotFoundError: Numpy needed for installation. "
+                     "Please install Numpy version <= 1.17.5.\n\n")
     sys.exit(1)
 
 if '1.17' not in numpy.__version__ and int(numpy.__version__.split(".")[2]) > 17:
-    sys.stderr.write("\nRuntimeError: Numpy <= 1.17.5 needed for installation."
-                     "Please downgrade your numpy and install numpy <= 1.17.5.\n\n")
+    sys.stderr.write("\nRuntimeError: Numpy version <= 1.17.5 needed for installation."
+                     "You are using Numpy version {}."
+                     "Please downgrade your numpy and install Numpy version <= 1.17.5.\n\n".format(numpy.__version__))
     sys.exit(1)
 
 
@@ -46,7 +80,7 @@ setup(name='sstmap',
       license='MIT',
       url='https://github.com/KurtzmanLab/SSTMap',
       platforms=['Linux', 'Mac OS X',],
-      python_requires='<3.8',
+      python_requires='<=3.7',
       install_requires=['parmed==3.2.0','matplotlib==2.2.3','mdtraj','numpy<=1.17.5'],
       setup_requires=['numpy<=1.17.5'],
       packages=find_packages(),
